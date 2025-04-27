39f870b27b7e78dea2c2390822459f91cc05805d
kellytran <kellytran300@gmail.com>
2025-04-27 21:35:27 +0200

    commit

--- packages/kwm24/Resources/Public/JavaScript/Dist/scripts.js
+++ packages/kwm24/Resources/Public/JavaScript/Dist/scripts.js
@@ -4,3 +4,20 @@
  * Licensed under the GPL-2.0-or-later license
  */
 console.log("WE LOVE TYPO3");
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1, // <<< ÄNDERN auf 1!
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
